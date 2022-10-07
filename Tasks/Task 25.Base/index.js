a9b7d3e337a17c43f18b2a2cda12dcf927b9e4cd
Michail Kupreenko <mislepkov@gmail.com>
2022-10-07 21:39:14 +0300

    nask 25

--- Tasks/Task 25.Base/index.js
+++ Tasks/Task 25.Base/index.js
@@ -1,23 +1,73 @@
-var hours = 14 * 3600;
-var min = 50 * 60;
-var hours2 = 15 * 3600;
- var min2 = 50 * 60;
- var sum1 = hours + min;
- var sum2 = hours2 + min2;
- var div = sum2 - sum1;
- console.log(div);
+// var hours = 14 * 3600;
+// var min = 50 * 60;
+// var hours2 = 15 * 3600;
+//  var min2 = 50 * 60;
+//  var sum1 = hours + min;
+//  var sum2 = hours2 + min2;
+//  var div = sum2 - sum1;
+//  console.log(div);
 
- var a = 3;
- var b = 1;
- var s = a * b;
- var p = (a * 2) + (b * 2);
- var q = s / p;
- console.log(q);
+//  var a = 3;
+//  var b = 1;
+//  var s = a * b;
+//  var p = (a * 2) + (b * 2);
+//  var q = s / p;
+//  console.log(q);
 
- var a = 1000;
- var b = (1000 / 100) * 10;
- var year1 = b + a;
- var year2 = ((a + year1) / 100) * 10;
- var pyear2 = a + year2;
- var p = (year1 + year2 + year3) - a;
- console.log(p);
+//  var a = 1000;
+//  var b = (1000 / 100) * 10;
+//  var year1 = b + a;
+//  var year2 = ((a + year1) / 100) * 10;
+//  var pyear2 = a + year2;
+//  var p = (year1 + year2 + year3) - a;
+//  console.log(p);
+
+var name = "Вася";
+const surname = "Пупкин";
+var height, weight;
+var height = 180;
+var weight = 0.89;
+var copy = weight * 100;
+console.log("Меня зовут", name, surname,".", "Я вешу" , weight, "ц.", "Мой рост -", height);
+var weight = 0.89 + 0.1;
+var height = 180 - 2;
+console.log( name, surname, "потолстел и стоптался");
+var name = "Петя";
+var weight = 0.89 * 2;
+console.log("Теперь я", name, surname, "и вешу в 111.24% больше, чем в молодости .")
+
+//Task2 
+var side = 2;
+var p = side *4;
+console.log('Перимерт квадрата со стороной', side, '=', p);
+var radius = 4;
+var s = Math.PI *(radius*radius);
+console.log("площадь круга с радиусом", radius, "равна", s );
+var a = 5;
+var b = 7;
+var c = (a * a) + (b * b);
+console.log("гипотенуза равна ", c );
+var q = 6;
+var t = 7;
+var u = q;
+var q = t;
+t = u;
+console.log(q,t,u);
+
+var a = 123;
+var b = a % 10;
+console.log(b);
+var c = (( a % 100) - b) / 10;
+console.log(c);
+var d = (a - (c * 10) - b) / 100;
+console.log(d);
+var q = b + c + d;
+console.log(b + c + d);
+
+var a = 982;
+var b = a % 10;
+var c = ((a % 100) - b) / 10;
+var d = (a - (c * 10) - b) / 100;
+var q = (b*100) + (c*10) + d;
+console.log(q);
+
